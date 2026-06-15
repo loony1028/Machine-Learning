84d0171ecb68b7f49d3e0e063dd0c58c4e75698d
luca <165953410+loony1028@users.noreply.github.com>
2026-06-15 23:46:29 +0100

    add training loop for multiple classifiers (LR, RFC, DTC, XGB) and return trained models dict

--- Insurance Claims/src/train.py
+++ Insurance Claims/src/train.py
@@ -15,35 +15,6 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-# %%
-# ==== Training Model =====
-'''
-def train_lr_model(X_train, y_train):
-    lr_model = LogisticRegression(class_weight='balanced', random_state=42)
-    lr_model.fit(X_train, y_train)
-
-    return lr_model
-
-def train_rfc_model(X_train, y_train)
-    rfc_model = RandomForestClassifier(class_weight='balanced', random_state=42)
-    rfc_model.fit(X_train, y_train)
-
-    return rfc_model
-
-def train_dfc_model(X_train, y_train):
-    dfc_model = DecisionTreeClassifier(class_weight='balanced', random_state=42)
-    dfc_model.fit(X_train, y_train)
-
-    return dfc_model
-
-def train_xgboost_model(X_train, y_train):
-    xgb_model = XGBClassifier()
-    xgb_model.fit(X_train, y_train)
-
-    return xgb_model
-
-'''
-
 
 # %%
 # ==== Training Model =====
@@ -55,9 +26,12 @@ models = {
 }
 
 def train_model(X_train, y_train):
-    for model in models:
+    trained_model = {}
+
+    for name, model in models.items():
         model.fit(X_train, y_train)
+        trained_model[name] = model
         
 
-    return model
+    return trained_model
 
